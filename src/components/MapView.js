10c99037593300b22100641022d07946a4a80ffa
Gali1917 <carlgale93@hotmail.com>
2023-02-08 14:21:25 -0500

    Add/Gali1917/Imagen del grupo, borde mapa

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -5,16 +5,24 @@ import "../styles/mapView.css";
 
 const MapView = () => {
     return <MapContainer className="mapa" center={[4.7350214, -74.1071767]} zoom={17} scrollWheelZoom={false}>
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <Marker position={[4.7350214, -74.1071767]}>
-      <Popup>
-        Capoeira Morcegin <br /> Puño, pata y porro <br /> Domingos 10:00-12:00
-      </Popup>
-    </Marker>
-  </MapContainer>
+        <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[4.7350214, -74.1071767]}>
+            <Popup className="popup-mapa">
+                <div>
+                    <img src="https://scontent.fbog2-5.fna.fbcdn.net/v/t39.30808-6/304945393_10158912451718085_4106091627299183990_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=iy7wKk3841EAX-1WsVJ&_nc_ht=scontent.fbog2-5.fna&oh=00_AfA1TtZpGHglrrBsbx_ktfcB8cNg5nG4JInAYiEdH_4s9g&oe=63E8E59D"></img>
+                </div>
+                <div>
+
+                    Capoeira Morcego <br />
+                    Domingos 10:00-12:00 <br />
+                    <a href="https://wa.link/mfw4zo" target="_blank">Whatsapp</a>
+                </div>
+            </Popup>
+        </Marker>
+    </MapContainer>
 }
 
 export default MapView;
